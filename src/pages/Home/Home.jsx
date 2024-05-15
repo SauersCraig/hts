78a88c008fa11dafa421a5ca277bd53e492e5a19
Craig Stueber <craigstueber@gmail.com>
2024-05-15 09:47:13 -0400

    Home Page Coming Soon Complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,9 +5,14 @@ export function Home() {
   return (
     <div className="comingSoonPage">
       <p className="comingSoon">Coming Soon Hot Tomato Summer</p>
-      
+      <a
+        href="https://dukesmayo.com/pages/htsreg"
+        className="htsLink"
+        target="_blank"
+      >
+        Sign Up for Hot Tomato Summer
+      </a>
       <img src={mainLogo} className="homeLogo" />
-      
     </div>
   );
 }
