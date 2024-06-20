cd0e083deb37753b956616a42998dbb452314a66
Craig Stueber <cstueber@sauerbrands.com>
2024-06-20 15:08:55 -0400

    stuff

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,6 @@
 import mainLogo from "../../assets/UniversalHTSLogo.png";
 import "./Home.styles.css";
-import Reel from "../../assets/HotTomatoSummerReel.mp4";
-import HTSLogo from "../../assets/HTSLogo (png).png";
-import HTSLogo2 from "../../assets/HTSLogo2 (png).png";
-import HTSLogo3 from "../../assets/HTSLogo3.png";
-import HTSLogo4 from "../../assets/HTSLogo4.png";
-import HTSLogoSocialPost from "../../assets/HTSLogoSocialPost.jpg";
-import HTSLogoSocialPost2 from "../../assets/HTSLogoSocialPost2.jpg";
+
 export function Home() {
   return (
     <div className="comingSoonPage">
