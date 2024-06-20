cc1f6f1ac99c4529c7ed91a47e3115d5c6ca10b5
Craig Stueber <cstueber@sauerbrands.com>
2024-06-20 09:51:57 -0400

    stuff

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,7 +26,10 @@ export function Home() {
           <div className="downloadCard">
             <img src={HTSLogo} className="downloadImg"></img>
             <p>Hot Tomato Summer Logo with date (.png) </p>
-            <a href="/src/assets/HTSLogo%20(png).png" download>
+            <a
+              href="https://hottomatosummer.com/assets/HTSLogo%20(png)-DK7Dw7ck.png"
+              download
+            >
               Download
             </a>
           </div>
@@ -35,7 +38,10 @@ export function Home() {
           <div className="downloadCard">
             <img src={HTSLogo2} className="downloadImg"></img>
             <p>Hot Tomato Summer Logo without date (.png) </p>
-            <a href="/src/assets/HTSLogo2%20(png).png" download>
+            <a
+              href="https://hottomatosummer.com/assets/HTSLogo2%20(png)-Bn1_X17a.png"
+              download
+            >
               Download
             </a>
           </div>
@@ -44,7 +50,10 @@ export function Home() {
           <div className="downloadCard">
             <img src={HTSLogo3} className="downloadImg"></img>
             <p>Hot Tomato Summer Logo version 2 with date (.png) </p>
-            <a href="/assets/HTSLogo3-DiwW_QUf.png" download>
+            <a
+              href="https://hottomatosummer.com/assets/HTSLogo3-DiwW_QUf.png"
+              download
+            >
               Download
             </a>
           </div>
@@ -53,7 +62,10 @@ export function Home() {
           <div className="downloadCard">
             <img src={HTSLogo4} className="downloadImg"></img>
             <p>Hot Tomato Summer Logo version 2 without date (.png) </p>
-            <a href="/src/assets/HTSLogo4.png" download>
+            <a
+              href="https://hottomatosummer.com/assets/HTSLogo4-DRJqUVif.png"
+              download
+            >
               Download
             </a>
           </div>
@@ -62,7 +74,10 @@ export function Home() {
           <div className="downloadCard">
             <img src={HTSLogoSocialPost} className="downloadImg"></img>
             <p>Hot Tomato Summer social post (.jpg) </p>
-            <a href="/src/assets/HTSLogoSocialPost.jpg" download>
+            <a
+              href="https://hottomatosummer.com/assets/HTSLogoSocialPost-BRSmX3fr.jpg"
+              download
+            >
               Download
             </a>
           </div>
@@ -71,7 +86,10 @@ export function Home() {
           <div className="downloadCard">
             <img src={HTSLogoSocialPost2} className="downloadImg"></img>
             <p>Hot Tomato Summer social post version 2 (.jpeg) </p>
-            <a href="/src/assets/HTSLogoSocialPost2.jpg" download>
+            <a
+              href="https://hottomatosummer.com/assets/HTSLogoSocialPost2-kbBQGmK0.jpg"
+              download
+            >
               Download
             </a>
           </div>
@@ -82,7 +100,10 @@ export function Home() {
               <source src={Reel} type="video/mp4"></source>
             </video>
             <p>Hot Tomato Summer Reel (mp4) </p>
-            <a href="/src/assets/HotTomatoSummerReel.mp4" download>
+            <a
+              href="https://hottomatosummer.com/assets/HotTomatoSummerReel-DFA81biD.mp4"
+              download
+            >
               Download
             </a>
           </div>
