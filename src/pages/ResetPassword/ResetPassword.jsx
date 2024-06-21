1d22fb2b342e31e31b0cf2d4eb3876d6b38e2678
Craig Stueber <cstueber@sauerbrands.com>
2024-06-21 13:07:37 -0400

    navigate path added to reset password

--- src/pages/ResetPassword/ResetPassword.jsx
+++ src/pages/ResetPassword/ResetPassword.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { supabase } from "../../client";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./ResetPassword.styles.css";
 export function ResetPassword() {
   const [password, setPassword] = useState({
     pass: "",
     confirmPass: "",
   });
+  let navigate = useNavigate();
   function handleChange(event) {
     const value = event.target.value;
     setPassword({ ...password, [event.target.name]: value });
@@ -14,11 +16,12 @@ export function ResetPassword() {
   const confirmPasswords = async () => {
     const pass = password.pass;
     const confirmPass = password.confirmPass;
-
+    let path = "/GoldenTomato";
     if (pass !== confirmPass) return alert("Your passwords do not match");
     const { data: resetData, error } = await supabase.auth.updateUser({
       password: pass,
     });
+    navigate(path);
   };
   return (
     <div className="formContainer">
