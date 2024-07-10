a172a91d50dd16c1c60352e2ceaddd26d8d12806
Craig Stueber <craigstueber@gmail.com>
2024-07-10 12:30:31 -0400

    date moved to only render once

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -4,10 +4,11 @@ import { useNavigate } from "react-router-dom";
 import { RestContext } from "../../RestContext";
 import upIcon from "../../assets/icons/angle-up.svg";
 import downIcon from "../../assets/icons/angle-down.svg";
-function Accordion({ i }) {
+function Accordion({ i, newDate }) {
   const [isActive, setIsActive] = useState(false);
   const { resName } = useContext(RestContext);
   const [restName, setRestName] = resName;
+
   let navigate = useNavigate();
   const handleRouteChange = (item) => {
     let rest = item;
@@ -74,18 +75,23 @@ function Accordion({ i }) {
                 </div>
               </div>
             </div>
-            {/* <div className="goldenTomAccordion">
-              <h3>Votes for Golden Tomato</h3>
-              {i.votes > 0 ? <p>{i.votes}</p> : <p>0</p>}
-            </div> */}
+            <div>
+              {newDate < 18 ? (
+                <></>
+              ) : (
+                <div className="goldenTomAccordion">
+                  <h3>Votes for Golden Tomato</h3>
+                  {i.votes > 0 ? <p>{i.votes}</p> : <p>0</p>}
+                  <button
+                    onClick={() => handleRouteChange(i)}
+                    className="btnHTS voteBtn"
+                  >
+                    Vote
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
-
-          {/* <button
-            onClick={() => handleRouteChange(i)}
-            className="btnHTS voteBtn"
-          >
-            Vote
-          </button> */}
         </div>
       )}
     </div>
--- src/components/RestSection/RestSection.jsx
+++ src/components/RestSection/RestSection.jsx
@@ -1,11 +1,12 @@
 import "./RestSection.styles.css";
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 
 import { RestContext } from "../../RestContext";
 import Accordion from "../Accordion/Accordion";
 export function RestSection({ rests }) {
   const { resName } = useContext(RestContext);
   const [restName, setRestName] = resName;
+  const [newDate, setNewDate] = useState();
   const restaurants = rests.sort((a, b) => {
     const nameA = a.name.toUpperCase(); // ignore upper and lowercase
     const nameB = b.name.toUpperCase(); // ignore upper and lowercase
@@ -19,13 +20,18 @@ export function RestSection({ rests }) {
     // names must be equal
     return 0;
   });
-
+  useEffect(() => {
+    const today = new Date();
+    console.log(today);
+    const date = today.getDate();
+    setNewDate(date);
+  }, []);
   
   return (
     <div className="restContainer">
       {restaurants.map((i) => (
         <div key={i.id} className="restCard">
-          <Accordion i={i} />
+          <Accordion i={i} newDate={newDate} />
         </div>
       ))}
     </div>
